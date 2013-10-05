3717d6c878bbaef0c282320810bbb44bc9333a7e
Amazon <amazon.xia@cargosmart.com>
2013-10-05 18:28:15 +0800

    Initial commit

---
+++ rails_projects/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'fa706183939d604f09d73a03259e81392088bcf4a68c55c73a3b233bf83fe56519c62e5630cf1f96e606d9414b88916abe349d8de6e16c1a75ad6e3fbf366a6b'
